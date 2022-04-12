6e61e83543d23f612beadb0ef5d040b779e70e90
raul reymundi <kinskispiral@gmail.com>
2022-04-12 09:58:01 -0400

    commit tree 266a578ed35a0133bfa574c4979e893e940d90c0

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "Raul is the champion";
+
+
+document.body.appendChild(newHeader);
